3c76b86241282fb2b563c2179aa0e3fb3a4ccced
Johan Hidding <j.hidding@esciencecenter.nl>
2024-12-08 08:05:48 +0100

    day 8

--- src/Day08.jl
+++ src/Day08.jl
@@ -1,8 +1,56 @@
 # ~/~ begin <<docs/day08.md#src/Day08.jl>>[init]
 module Day08
 
+using .Iterators: filter, flatten, map, takewhile, countfrom
+
+function group(a::A; skip=()) where {A}
+    g = Dict{valtype(A),Vector{keytype(A)}}()
+    for (k, v) ∈ pairs(a)
+        v ∈ skip && continue
+        if v ∉ keys(g)
+            g[v] = []
+        end
+        push!(g[v], k)
+    end
+    return g
+end
+
+all_pairs(x) = ((i, j) for i in x for j in x if (i != j))
+
+function antinodes_1(inbounds)
+    f(a::CartesianIndex{2}, b::CartesianIndex{2}) =
+        (2a - b, 2b - a)
+
+    f(x::Vector{CartesianIndex{2}}) =
+        filter(inbounds, flatten(map(splat(f), all_pairs(x))))
+
+    return f
+end
+
+function antinodes_2(inbounds)
+    function f(a::CartesianIndex{2}, b::CartesianIndex{2})
+        d = b - a
+        one_way = (a - i*d for i in countfrom(0))
+        other_way = (b + i*d for i in countfrom(0))
+        return flatten((takewhile(inbounds, one_way),
+                        takewhile(inbounds, other_way)))
+    end
+
+    f(x::Vector{CartesianIndex{2}}) =
+        flatten(map(splat(f), all_pairs(x)))
+
+    return f
+end
+
 function main(io::IO)
-    return nothing
+    input = io |> readlines |> stack
+    g = group(input, skip=('.',))
+    inbounds(i) = checkbounds(Bool, input, i)
+    part1 = length(
+        values(g) .|> antinodes_1(inbounds) |> flatten |> unique)
+    part2 = length(
+        values(g) .|> antinodes_2(inbounds) |> flatten |> unique)
+    return part1, part2
 end
 
 end
